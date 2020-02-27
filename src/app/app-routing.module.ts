607737dd2f61cfa5a0f3b0a4fa6ff91aeae0073b
aniket17 <anidandawate1@gmail.com>
2020-02-27 23:51:50 +0530

    Added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { WelcomeComponent } from "./components/welcome/welcome.component";
+import { QuizComponent } from "./components/quiz/quiz.component";
 
 const routes: Routes = [
   { path: "home", component: WelcomeComponent },
+  { path: "quiz", component: QuizComponent },
   { path: "", redirectTo: "/home", pathMatch: "full" }
 ];
 

c4c33828297fa19b45fd564cc96db7b628ba0ac2
aniket17 <anidandawate1@gmail.com>
2020-02-27 23:22:43 +0530

    Added Timer component

--- src/app/components/timer/timer.component.ts
+++ src/app/components/timer/timer.component.ts
@@ -7,6 +7,40 @@ import { EventEmitter } from "@angular/core";
   styleUrls: ["./timer.component.css"]
 })
 export class TimerComponent implements OnInit {
+  timer: any = null;
+  startTime: Date;
+  endTime: Date;
+  ellapsedTime = "00:00";
+
+  @Input() duration: number = 70; //default value
+  @Output() timerElapsed = new EventEmitter();
   constructor() {}
-  ngOnInit() {}
+
+  ngOnInit() {
+    this.startTime = new Date();
+    this.ellapsedTime = "00:00";
+    this.timer = setInterval(() => {
+      this.tick();
+    }, 1000);
+  }
+  tick() {
+    const now = new Date();
+    const diff = (now.getTime() - this.startTime.getTime()) / 1000;
+    if (diff >= this.duration) {
+      clearInterval(this.timer);
+      this.timerElapsed.emit("timerElapsed");
+    }
+    this.ellapsedTime = this.parseTime(this.duration - diff);
+  }
+
+  parseTime(totalSeconds: number) {
+    if (totalSeconds < 0) {
+      return "00:00";
+    }
+    let mins: string | number = Math.floor(totalSeconds / 60);
+    let secs: string | number = Math.round(totalSeconds % 60);
+    mins = (mins < 10 ? "0" : "") + mins;
+    secs = (secs < 10 ? "0" : "") + secs;
+    return `${mins}:${secs}`;
+  }
 }
